4ae1c0969bf47fd40ee48871b86fe5590a2b31cc
Gabriel Dias <gabriel.dias025@hotmail.com>
2025-02-16 19:13:06 -0300

    Aprendendo acoes com fragments

--- AulaFragment/app/src/main/java/com/dias/aulafragment/fragments/ConversasFragment.kt
+++ AulaFragment/app/src/main/java/com/dias/aulafragment/fragments/ConversasFragment.kt
@@ -1,18 +1,41 @@
 package com.dias.aulafragment.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.dias.aulafragment.R
 
+//class ConversasFragment : Fragment(R.layout.fragment_conversas)
 class ConversasFragment : Fragment() {
+
+    private lateinit var btnExecutar : Button
+    private lateinit var textNome : TextView
+    private lateinit var editNome : EditText
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("ciclo_vida", "Fragment onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("ciclo_vida", "Fragment onCreate")
+    }
+
+    //Aqui é criada a visualização
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.i("Ciclo_vida", "Fragment onCreateView")
         val view = inflater.inflate(
             //Layout que vou utilizar
             R.layout.fragment_conversas,
@@ -22,8 +45,65 @@ class ConversasFragment : Fragment() {
         )
 
         //Processamento da visualização
+        //Recuperar os componentes da interface
+
+        btnExecutar = view.findViewById(R.id.btn_executar)
+        editNome = view.findViewById(R.id.edit_nome)
+        textNome = view.findViewById(R.id.text_nome)
+
         //view.findViewById<>()
 
+        btnExecutar.setOnClickListener {
+            val nome = editNome.text.toString()
+            textNome.text = nome
+        }
+
         return view
+    }
+
+    /*Depreciado
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+    */
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("ciclo_vida", "Fragment onViewCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("ciclo_vida", "Fragment onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ciclo_vida", "Fragment onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("ciclo_vida", "Fragment onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ciclo_vida", "Fragment onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("ciclo_vida", "Fragment onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ciclo_vida", "Fragment onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("ciclo_vida", "Fragment onDetach")
     }
 }
