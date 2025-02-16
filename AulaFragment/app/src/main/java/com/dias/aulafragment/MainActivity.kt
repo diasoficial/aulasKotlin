13dabb82b993d177cd8743484c3c490ffb112bb3
Gabriel Dias <gabriel.dias025@hotmail.com>
2025-02-15 21:03:24 -0300

    Fragment finalizado

--- AulaFragment/app/src/main/java/com/dias/aulafragment/MainActivity.kt
+++ AulaFragment/app/src/main/java/com/dias/aulafragment/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.dias.aulafragment
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dias.aulafragment.fragments.ChamadasFragment
+import com.dias.aulafragment.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnConversas: Button
+    private lateinit var btnChamadas: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,35 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnConversas = findViewById(R.id.btn_conversas)
+        btnChamadas = findViewById(R.id.btn_chamadas)
+
+        /*val fragmentManager = supportFragmentManager.beginTransaction()
+
+        //Diversas alterações em fragments
+
+        //Aqui recebe o fragmentContainerView
+        fragmentManager.add(R.id.fragment_conteudo, ConversasFragment())
+
+        fragmentManager.commit()*/
+
+        btnConversas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                //.add Adiciona uma certa fragment
+                //.replace Substitui uma certa fragment
+                //.remove
+                .replace(R.id.fragment_conteudo, ConversasFragment())
+                .commit()
+        }
+
+        btnChamadas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_conteudo, ChamadasFragment())
+                .commit()
         }
     }
 }
---
+++ AulaFragment/app/src/main/java/com/dias/aulafragment/fragments/ChamadasFragment.kt
@@ -0,0 +1,22 @@
+package com.dias.aulafragment.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dias.aulafragment.R
+
+class ChamadasFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_chamadas,
+            container,
+            false
+        )
+    }
+}
