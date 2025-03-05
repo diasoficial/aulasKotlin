c0f0c6c42d5ad25860959a8a8269b6a28628589d
Gabriel Dias <gabriel.dias025@hotmail.com>
2025-03-05 20:34:53 -0300

    Aula 119 - Classes Aninhadas e Classes Internas - OK

--- AulaComponentesListagemColecoes/app/src/main/java/com/dias/aulacomponenteslistagemcolecoes/teste/classes.kt
+++ AulaComponentesListagemColecoes/app/src/main/java/com/dias/aulacomponenteslistagemcolecoes/teste/classes.kt
@@ -1,5 +1,30 @@
 package com.dias.aulacomponenteslistagemcolecoes.teste
 
-fun main() {
+class Motorista(val nome: String) {
 
+    fun exibirDadosMotorista() = println("Motorista: $nome")
+
+    // Classe aninhada (Nested Class) = uma classe que está dentro de outra classe
+    /*class Caminhao(val nomeCaminhao: String) {
+        fun exibirDadosCaminhao() = println("Caminhao: $nomeCaminhao")
+    }*/
+
+    // Para acessar o nome que está dentro de motorista
+    inner class Caminhao(val nomeCaminhao: String) {
+        fun exibirDadosCaminhao() = println("Caminhao: $nomeCaminhao, Motorista: $nome")
+    }
+}
+
+fun main() {
+    //Usar com classe com inner
+    val motorista = Motorista("Gabriel")
+    val caminhao = motorista.Caminhao("FH 60")
+    caminhao.exibirDadosCaminhao()
+
+    //Usar com classe aninhada sem inner
+    /*val caminhao = Motorista.Caminhao("FH 60")
+    caminhao.exibirDadosCaminhao()*/
+
+    /*val motorista = Motorista("Gabriel")
+    motorista.exibirDadosMotorista()*/
 }
