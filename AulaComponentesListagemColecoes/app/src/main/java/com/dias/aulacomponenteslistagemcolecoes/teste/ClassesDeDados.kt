e85b4da5d3bb76dfae875c995ef916d9ecbff7f7
Gabriel Dias <gabriel.dias025@hotmail.com>
2025-03-05 21:05:01 -0300

    Aula 120 - Classes de Dados - OK

---
+++ AulaComponentesListagemColecoes/app/src/main/java/com/dias/aulacomponenteslistagemcolecoes/teste/ClassesDeDados.kt
@@ -0,0 +1,11 @@
+package com.dias.aulacomponenteslistagemcolecoes.teste
+
+//Estamos construindo um aplicativo de pergunta
+data class Pergunta(val pergunta: String, val respostaCerta: Int)
+
+fun main() {
+    val pergunta1 = Pergunta("Qual a pergunta?", 1)
+    val pergunta2 = Pergunta("Qual a pergunta?", 2)
+
+    println(pergunta1 == pergunta2)
+}
