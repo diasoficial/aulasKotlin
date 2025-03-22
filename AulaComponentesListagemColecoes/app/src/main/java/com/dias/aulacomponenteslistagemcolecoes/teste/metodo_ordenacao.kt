87a5b1e4113410895b3de13092d9b48f2573f00d
Gabriel Dias <gabriel.dias025@hotmail.com>
2025-03-21 22:40:37 -0300

    Aula 132 - Operacoes com Colecoes - Ordenacao - OK

--- AulaComponentesListagemColecoes/app/src/main/java/com/dias/aulacomponenteslistagemcolecoes/teste/metodo_ordenacao.kt
+++ AulaComponentesListagemColecoes/app/src/main/java/com/dias/aulacomponenteslistagemcolecoes/teste/metodo_ordenacao.kt
@@ -1,2 +1,15 @@
 package com.dias.aulacomponenteslistagemcolecoes.teste
 
+fun main() {
+    val listaNomes = listOf("Gabriel", "Ana", "Bruno", "Maria", "Zeus")
+    val listaNumeros = listOf(2,1,3)
+
+    //val novaListaNumeros = listaNumeros.sorted() // Ordenação ASC | A-Z | 0-9 |
+    //val novaListaNumeros = listaNumeros.sortedDescending() // Ordenação DESC | Z-A | 9-0 |
+
+    val ListaNomesAsc = listaNomes.sorted()
+    val ListaNomesDesc = listaNomes.sortedDescending()
+
+    println(ListaNomesAsc)
+    println(ListaNomesDesc)
+}
