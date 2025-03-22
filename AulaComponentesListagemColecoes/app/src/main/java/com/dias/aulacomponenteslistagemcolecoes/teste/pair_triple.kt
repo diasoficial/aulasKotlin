75577d6400723becae02a038f3a66b8068ce14e3
Gabriel Dias <gabriel.dias025@hotmail.com>
2025-03-21 22:47:33 -0300

    Aula 133 - Pair e Triple - OK

---
+++ AulaComponentesListagemColecoes/app/src/main/java/com/dias/aulacomponenteslistagemcolecoes/teste/pair_triple.kt
@@ -0,0 +1,13 @@
+package com.dias.aulacomponenteslistagemcolecoes.teste
+
+fun main() {
+    //val localizacao = Pair(10, 20)
+    //val localizacao = 10 to 20
+    val localizacao = Triple(10, 20, "Gabriel")
+
+
+    println(localizacao.first)
+    println(localizacao.second)
+    println(localizacao.third)
+
+}
